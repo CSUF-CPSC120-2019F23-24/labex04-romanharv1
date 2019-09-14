9e8c3a1b1c5d327f951b6230f7046dc86ffca89d
romanharv1 <romanharv1@csu.fullerton.edu>
2019-09-14 15:16:19 -0700

    Wrote sales prediction program

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,16 @@
 // Sales prediction
+#include <iostream>
+
+int main()
+{
+
+  const int SALES_YEAR1 = 2103419277; //sales last year
+  const int PERC_INCREASE_X100 = 18; //integer value for increase
+  long salesnew;
+  salesnew =  double(SALES_YEAR1) * double(PERC_INCREASE_X100) / 100 + SALES_YEAR1;
+
+  std::cout << "Last year's sales were $" << SALES_YEAR1 << std::endl;
+  std::cout << "This year's sales prediction: $" << salesnew;
+
+return 0;
+}
